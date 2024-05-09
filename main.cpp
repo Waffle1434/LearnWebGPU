a57774c0210a759d23e606ce8ac67c81ea1aeb66
Jason Deacutis <jdeacutis@inter-coastal.net>
2024-05-09 14:21:25 -0700

    GLFW Window

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <GLFW/glfw3.h> // Native Window
 
 int main (int, char**) {
-    std::cout << "Hello, world!" << std::endl;
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return 1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "WebGPU", NULL, NULL);
+    if (!window) {
+        std::cerr << "Failed to create window" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
+
+    // Main Loop
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
     return 0;
 }
